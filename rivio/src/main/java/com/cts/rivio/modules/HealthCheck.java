ef203d2a1f8be5f3b3336791ec5a534f7c2452a7
Paliwal <2479339@cognizant.com>
2026-03-27 17:06:39 +0530

    added new api response for health api

--- rivio/src/main/java/com/cts/rivio/modules/HealthCheck.java
+++ rivio/src/main/java/com/cts/rivio/modules/HealthCheck.java
@@ -1,14 +1,15 @@
 package com.cts.rivio.modules;
 
 
+import com.cts.rivio.core.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheck {
     @GetMapping("/health")
-    public String checkHealth(){
-        return "All Systems are working fine";
+    public ResponseEntity<ApiResponse> checkHealth(){
+        return ResponseEntity.ok(ApiResponse.success(null,"All Systems are healthy"));
     }
-
 }
